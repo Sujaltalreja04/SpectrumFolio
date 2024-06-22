e8fbcde843ee60fc618c680c56705b0ec8a66146
Sujaltalreja04 <128150608+Sujaltalreja04@users.noreply.github.com>
2024-06-22 21:28:02 +0530

    file added

---
+++ src/content_option.js
@@ -0,0 +1,185 @@
+const logotext = "Sujal";
+const meta = {
+    title: "SpectrumFolio",
+    description: "I’m Sujal kishore kumar talreja data scientist _ Full stack devloper,currently pursuing Bscit from Ganpat University",
+};
+
+const introdata = {
+    title: "I’m Sujal kishore kumar talreja",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites and web apps",
+        third: "I am business analyst, data analyst, data scientist",
+        fourth: "I develop various coding projects",
+       
+    },
+    description: "A self-taught Data Analyst and BSc IT student from Ganpat University, committed to excelling in the field of business analytics and data science. With a mindset focused on creating a life rich with knowledge and meaningful experiences, I am currently pursuing a BSc in IT from Ganpat University. My goal is to become a successful data analyst or cyber security expert. Visit my GitHub and LinkedIn profiles for updates on my latest achievements and projects.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: " About my self",
+    aboutme: "My name is Sujal Kishore Kumar Talreja. I am a 21-year-old Indian software developer currently pursuing a BSc in IT from Ganpat University. I have developed skills in HTML, CSS, JavaScript, the MERN stack, Python, NumPy, Pandas, Power BI, automation, and Tableau.",
+};
+const worktimeline = [{
+        jobtitle: "Data scientist",
+        where: "YBI Foundation",
+        date: "May 2024 - Present",
+    },
+    {
+        jobtitle: "Data scientist",
+        where: "360digitmg",
+        date: "May 2024 - Present",
+    },
+    {
+        jobtitle: "Power Bi Developer",
+        where: "CognoRise InfoTech",
+        date: "May 2024 - Jun 2024",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Djano",
+        value: 85,
+    },
+    {
+        name: "Numpy",
+        value: 85,
+    },
+    {
+        name: "Panda",
+        value: 100,
+    },
+    {
+        name: "Power bi",
+        value: 100,
+    },
+    {
+        name: "English communication",
+        value: 100,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Mern StacK Websites",
+        description: "I have deep profieciency in making mern stack websites through my skills ",
+    },
+    {
+        title: "Seo Expert",
+        description: "I have deep skills set in market analysis and seo expert ",
+    },
+    {
+        title: "Business and Data analytics",
+        description: "I have deep profieciency in market analysis, data analysis, business analysis, python , numpy, panda, logical thinking",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://github.com/webcoder904/Speech-recognition/blob/main/resize%20speech%20regonition.png?raw=true",
+        description: "This is a speech recognition website where website can convert your speech into text",
+        link: "https://webcoder904.github.io/Speech-recognition/",
+    },
+    {
+        img: "https://github.com/webcoder904/SUJAL-STICK-GAME/blob/main/resize.png?raw=true",
+        description: "Create a captivating game where players have to pass obstacles using a black stick.",
+        link: "https://webcoder904.github.io/SUJAL-STICK-GAME/",
+    },
+    {
+        img: "https://github.com/webcoder904/Sujal-menja/blob/main/menja%20resize.png?raw=true",
+        description: "A simple smashing game where players have to smash popping 3D cubes with the help of the cursor.",
+        link: "https://webcoder904.github.io/Sujal-menja/",
+    },
+    {
+        img: "https://github.com/webcoder904/Crossy-road/blob/main/resize%20crossy%20road.png?raw=true",
+        description: "Crossy road game in which players have to pass road obstacles to finish the game",
+        link: "#",
+    },
+    {
+        img: "https://github.com/webcoder904/2048-game/blob/main/Screenshot%202024-05-07%20101729.png?raw=true",
+        description: "2048 is a single-player sliding tile puzzle video game",
+        link: "https://codepen.io/Sujal-Talreja/pen/gOyKbda",
+    },
+    {
+        img: "https://github.com/webcoder904/Mole-Game/blob/main/resize%20mole%20game.png?raw=true",
+        Title: "Web app game",
+        description: "A mouse killing game",
+        link: "https://webcoder904.github.io/Mole-Game/",
+    },
+
+    {
+        img: "https://i.ibb.co/BsdTkVN/Screenshot-2024-06-22-201704.png",
+        description: "A portfolio website made through mern stack technology",
+        link: "https://webcoder904.github.io/ZXC/",
+    },
+    {
+        img: "https://i.ibb.co/frN5JNG/Screenshot-2024-06-22-202428.png",
+        description: "Terminal or chatbot based portfolio website",
+        link: "https://terminal-chatbot-react-app.vercel.app/",
+    },
+    {
+        img: "https://i.ibb.co/Jv6HZTG/Screenshot-2024-06-22-202633.png",
+        description: "Code nebula legacy portfolio website",
+        link: "https://webcoder904.github.io/Code-Nebula/",
+    },
+    {
+        img: "https://i.ibb.co/cr2dw2z/Screenshot-2024-06-22-203459.png",
+        description: "Eranix Virtual Assistant",
+        link: "https://webcoder904.github.io/Voice-Virtual-Assistant/",
+    },
+    {
+        img: "https://github.com/webcoder904/Weather-web-application/blob/main/reseize%20weather%20app.png?raw=true",
+        description: "Sujal weather web app",
+        link: "https://webcoder904.github.io/Weather-web-application/",
+    },
+    {
+        img: "https://i.ibb.co/GHhKzbZ/Screenshot-2024-06-22-202912.png",
+        description: "Google Gemini Ai Powered Clone",
+        link: "https://sujal-gemini-react-web-application.vercel.app/",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "sujaltalreja04@gmail.com",
+    YOUR_FONE: "7574021120",
+    YOUR_Address: " Ahmedabad Gujarat India 382340 ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/Sujaltalreja04",
+    whatsapp: "https://api.whatsapp.com/send/?phone=7574021120&text&type=phone_number&app_absent=0",
+    linkedin: "https://www.linkedin.com/in/sujal-kishore-kumar-talreja-65975b216/",
+    twitter: "https://x.com/sujal_k65124?t=K_ltu9Rl9PTBUWu1jnO6Ow&s=09",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
---
+++ src/pages/contact/index.js
@@ -0,0 +1,183 @@
+import React, { useState } from "react";
+import * as emailjs from "emailjs-com";
+import "./style.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { meta } from "../../content_option";
+import { Container, Row, Col, Alert } from "react-bootstrap";
+import { contactConfig } from "../../content_option";
+
+export const ContactUs = () => {
+  const [formData, setFormdata] = useState({
+    email: "",
+    name: "",
+    message: "",
+    loading: false,
+    show: false,
+    alertmessage: "",
+    variant: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setFormdata({ ...formData, loading: true });
+
+    // Handle Formspree submission
+    const form = e.target;
+    const data = new FormData(form);
+    const formspreeResponse = await fetch(form.action, {
+      method: form.method,
+      body: data,
+      headers: {
+        Accept: "application/json",
+      },
+    });
+
+    if (formspreeResponse.ok) {
+      // Formspree success
+      console.log("Formspree success!");
+      setFormdata({
+        loading: false,
+        alertmessage: "SUCCESS! Thank you for your message.",
+        variant: "success",
+        show: true,
+      });
+    } else {
+      // Formspree error
+      console.error("Formspree error:", formspreeResponse.statusText);
+      setFormdata({
+        alertmessage: `Failed to send message via Formspree: ${formspreeResponse.statusText}`,
+        variant: "danger",
+        show: true,
+      });
+    }
+
+    // Handle emailjs submission
+    const templateParams = {
+      from_name: formData.email,
+      user_name: formData.name,
+      to_name: contactConfig.YOUR_EMAIL,
+      message: formData.message,
+    };
+
+    emailjs
+      .send(
+        contactConfig.YOUR_SERVICE_ID,
+        contactConfig.YOUR_TEMPLATE_ID,
+        templateParams,
+        contactConfig.YOUR_USER_ID
+      )
+      .then(
+        (result) => {
+          console.log("EmailJS success:", result.text);
+          // You may choose to set additional state here if needed
+        },
+        (error) => {
+          console.error("EmailJS error:", error.text);
+          // You may choose to set additional state here if needed
+        }
+      );
+  };
+
+  const handleChange = (e) => {
+    setFormdata({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <HelmetProvider>
+      <Container>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>{meta.title} | Contact</title>
+          <meta name="description" content={meta.description} />
+        </Helmet>
+        <Row className="mb-5 mt-3 pt-md-3">
+          <Col lg="8">
+            <h1 className="display-4 mb-4">Contact Me</h1>
+            <hr className="t_border my-4 ml-0 text-left" />
+          </Col>
+        </Row>
+        <Row className="sec_sp">
+          <Col lg="12">
+            <Alert
+              variant={formData.variant}
+              className={`rounded-0 co_alert ${formData.show ? "d-block" : "d-none"}`}
+              onClose={() => setFormdata({ show: false })}
+              dismissible
+            >
+              <p className="my-0">{formData.alertmessage}</p>
+            </Alert>
+          </Col>
+          <Col lg="5" className="mb-5">
+            <h3 className="color_sec py-4">Get in touch</h3>
+            <address>
+              <strong>Email:</strong>{" "}
+              <a href={`mailto:${contactConfig.YOUR_EMAIL}`}>
+                {contactConfig.YOUR_EMAIL}
+              </a>
+              <br />
+              <br />
+              {contactConfig.hasOwnProperty("YOUR_FONE") && (
+                <p>
+                  <strong>Phone:</strong> {contactConfig.YOUR_FONE}
+                </p>
+              )}
+            </address>
+            <p>{contactConfig.description}</p>
+          </Col>
+          <Col lg="7" className="d-flex align-items-center">
+            <form onSubmit={handleSubmit} className="contact__form w-100" action="https://formspree.io/f/moqgbedp" method="POST">
+              <Row>
+                <Col lg="6" className="form-group">
+                  <input
+                    className="form-control"
+                    id="name"
+                    name="name"
+                    placeholder="Name"
+                    value={formData.name || ""}
+                    type="text"
+                    required
+                    onChange={handleChange}
+                  />
+                </Col>
+                <Col lg="6" className="form-group">
+                  <input
+                    className="form-control rounded-0"
+                    id="email"
+                    name="email"
+                    placeholder="Email"
+                    type="email"
+                    value={formData.email || ""}
+                    required
+                    onChange={handleChange}
+                  />
+                </Col>
+              </Row>
+              <textarea
+                className="form-control rounded-0"
+                id="message"
+                name="message"
+                placeholder="Message"
+                rows="5"
+                value={formData.message}
+                onChange={handleChange}
+                required
+              ></textarea>
+              <br />
+              <Row>
+                <Col lg="12" className="form-group">
+                  <button className="btn ac_btn" type="submit">
+                    {formData.loading ? "Sending..." : "Send"}
+                  </button>
+                </Col>
+              </Row>
+            </form>
+          </Col>
+        </Row>
+      </Container>
+      <div className={formData.loading ? "loading-bar" : "d-none"}></div>
+    </HelmetProvider>
+  );
+};
